c45d67be37ac7db8e80b0d19cadad9250dfe27fd
Zyzzmma <bogoczmateusz2010@gmail.com>
2024-10-19 09:55:28 +0200

    bot.py

---
+++ bot.py
@@ -0,0 +1,27 @@
+import discord
+from generator_hasel import gen_pass
+# Zmienna intencje przechowuje uprawnienia bota
+intents = discord.Intents.default()
+# Włączanie uprawnienia do czytania wiadomości
+intents.message_content = True
+# Tworzenie bota w zmiennej klienta i przekazanie mu uprawnień
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Zalogowaliśmy się jako {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$hello'):
+        await message.channel.send("Cześć!")
+    elif message.content.startswith('$bye'):
+        await message.channel.send("\U0001f642")   
+    elif message.content.startswith("$haslo"):
+        await message.channel.send(gen_pass(10))     
+    else:
+        await message.channel.send(message.content)
+
+client.run("")

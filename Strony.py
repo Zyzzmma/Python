79e88957dad2a825ea603c366c4fad5ed8a980f1
Zyzzmma <bogoczmateusz2010@gmail.com>
2024-11-09 10:21:17 +0100

    Create Strony.py

---
+++ Strony.py
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pl">
+    <head>
+        <meta charset="utf-8">
+        <title>Moja pierwsza strona</title>
+        <link href="style.css" rel="stylesheet">
+    </head>
+    <body>
+        <h1>To jest testowa strona</h1>
+        <p>To tylko akapit tekstu.</p>
+        <h2>A to jest nieco mniejszy nagłówek.</h2>
+        <ul>
+            <li>To jest element listy wypunktowanej (pierwszy)</li>
+            <li>a to jest drugi element.</li>
+        </ul>
+        <img src= "graj.png" alt="Przycisk rozpoczęcia gry">
+    </body>
+</html>
+
+
+
+
+
+body {
+    background-color: aliceblue;
+    font-size: 20px;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+
+h1 {
+    color: blue;
+    background-color: yellow;
+    border-radius: 20px;
+    padding: 20px;
+    margin-bottom: 50px;
+}
+
+p {
+    color: green;
+}
+
+ul {
+    list-style-type: '- ';
+}
